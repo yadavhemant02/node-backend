f179bed7289e0d5726306fa7a6786ec1f75bcd76
yadavhemant02 <yadavhemant9719@gmail.com>
2026-01-12 17:07:30 +0530

    hemant62

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const app = require('./app');
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 
 // const PORT = process.env.PORT || 3000;
 // const BIND_HOST = process.env.HOST || '0.0.0.0';
@@ -11,26 +11,30 @@ const app = require('./app');
 // const NODE_ENV = process.env.NODE_ENV || 'development';
 
 // Connect to MongoDB on startup
-let isConnected = false;
-async function connectToMongoDB() {
-  try {
-    await mongoose.connect('mongodb+srv://yadavhemant9719_db_user:Hemant$9719@cluster0.nt0ymv8.mongodb.net/?appName=Cluster0/lerning', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    isConnected = true;
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-  }
-}
+// let isConnected = false;
+// async function connectToMongoDB() {
+//   try {
+//     await mongoose.connect('mongodb+srv://yadavhemant9719_db_user:Hemant$9719@cluster0.nt0ymv8.mongodb.net/?appName=Cluster0/lerning', {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     });
+//     isConnected = true;
+//     console.log('Connected to MongoDB');
+//   } catch (error) {
+//     console.error('Error connecting to MongoDB:', error);
+//   }
+// }
 
-app.use(async (req, res, next) => {
-  if (!isConnected) {
-    await connectToMongoDB();
-  } 
-  next();
-});
+connectDB()
+    .then(() => {
+       console.log('Starting server...');
+    })
+    .catch((error) => {
+        console.error('✗ Failed to start server:', error.message);
+        console.error('Please ensure MongoDB is running or set MONGODB_URI environment variable');
+        process.exit(1);
+    });
+
 
 
 // Export app for Vercel serverless and other runtimes
