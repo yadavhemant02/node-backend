c4f717e7201cb44f4d07b37433afead9a239a6f0
yadavhemant02 <yadavhemant9719@gmail.com>
2026-01-12 17:17:05 +0530

    hemant6234

--- app.js
+++ app.js
@@ -50,4 +50,41 @@ app.use('/api/assignments', assignmentRoutes);
 const quizeRoutes = require('./route/quizeRoutes');
 app.use('/api/quizes', quizeRoutes);
 
+app.get('/swagger.json', (req, res) => {
+  res.json(swaggerDocument);
+});
+
+app.get('/api-docs', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.send(`
+<!DOCTYPE html>
+<html>
+<head>
+  <title>API Docs</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+</head>
+<body>
+<div id="swagger-ui"></div>
+
+<script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+<script src="https://unpkg.com/swagger-ui-dist/swagger-ui-standalone-preset.js"></script>
+
+<script>
+window.onload = function () {
+  SwaggerUIBundle({
+    url: "/swagger.json",
+    dom_id: "#swagger-ui",
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    layout: "StandaloneLayout"
+  });
+};
+</script>
+</body>
+</html>
+`);
+});
+
 module.exports = app;
